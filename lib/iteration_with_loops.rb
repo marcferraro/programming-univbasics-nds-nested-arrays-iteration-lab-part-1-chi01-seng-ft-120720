b0351d329b6d698e7b7019e6b13e279bff580995
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-23 23:13:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,5 +8,11 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index] [element_index] 
+    if src[row_index] [element_index] % 2 == 0
+      p src[row_index] [element_index]
+    end
+    element_index += 1
+  end
+  row_index += 1
+end
 end
